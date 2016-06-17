785acf27133d7fb68621da98397442d66caa227d
Marina Shchukina <shchukina.marina@gmail.com>
2016-06-17 23:38:05 +0100

    Tidy up sample usage

---
+++ example.php
@@ -0,0 +1,10 @@
+<?php
+	require_once('lib/GooglePlacesSearch.php');
+
+	$apiKey = 'AIzaSyCcnjLmWQYr0eP3_Er1DwyBOlKo6gGjDng'; //'Your Google Places API Key';
+	$keyword = 'Mandela'; //For example, 'Mandela' will search by places named by Mandela
+
+	// Coordinates provided in the input file should be set as google.maps.LatLng objects
+	$mandela_search = new GooglePlacesSearch($apiKey, $keyword);
+
+	$mandela_search->Search();
