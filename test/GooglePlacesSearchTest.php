a0252c9004c8fb978885fbc39d70ba8b755bd214
Marina Shchukina <shchukina.marina@gmail.com>
2016-06-18 16:23:19 +0100

    Added unit tests skeleton file

---
+++ test/GooglePlacesSearchTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'src/GooglePlacesSearch.php';
+
+class GooglePlacesSearchTest extends PHPUnit_Framework_TestCase {
+    public function testGetAreas() {
+
+    }
+
+    public function testQueryApi() {
+
+    }
+
+    public function testSetRadius() {
+
+    }
+
+    public function testSetLanguage() {
+
+    }
+}
